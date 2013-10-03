59cde1a6d72e83b6c4fb9ea4f8e37e12c64d7c45
michtchakm <michaelmicht@gmail.com>
2013-10-03 08:39:27 -0400

    created for loop

---
+++ NumberGuessingGame/NumberGuessingGame/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NumberGuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int guesses = 7;
+            int number;
+            
+            {
+                for(int i = 1; i < 100; i++);
+            }
+            
+                string input = Console.ReadLine();
+                Console.WriteLine("I'm thinking of a number between 1 and 100");
+
+        }
+    }
+}
