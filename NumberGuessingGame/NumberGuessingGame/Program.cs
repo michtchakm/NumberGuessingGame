80dc44e6fa8c3d1e385de3e97ab14b3dd7cc8c04
michtchakm <michaelmicht@gmail.com>
2013-10-28 10:08:30 -0400

    started if statements

--- NumberGuessingGame/NumberGuessingGame/Program.cs
+++ NumberGuessingGame/NumberGuessingGame/Program.cs
@@ -9,12 +9,34 @@ namespace NumberGuessingGame
     {
         static void Main(string[] args)
         {
+            int number;
+            int guess;
             {
                 for (int i = 1; i < 100; i++);
             }
-                string input = Console.ReadLine();
-                Console.WriteLine("I'm thinking of a number between 1 and 100");
-                Console.WriteLine("Is it ");
+                Console.WriteLine("I'm thinking of a number between 1 and 100. What is it?");
+                string input = Console.ReadLine(); // Accept a string from the user
+                string input.ToInt32 = guess;
+
+                // if (input == "y")
+                // {
+                // do something
+                // }
+
+                if (guess < number)
+                {
+                    Console.WriteLine("H");
+                }
+
+                if (guess > number)
+                {
+                    Console.WriteLine("L");
+                }
+
+                if (guess == number)
+                {
+                    Console.WriteLine("E");
+                }
         }
     }
 }
